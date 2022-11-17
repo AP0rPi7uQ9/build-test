7a91ec3a8541e97134de08427be6d70db4c31293
AP0rPi7uQ9 <56527731+AP0rPi7uQ9@users.noreply.github.com>
2022-11-17 10:35:37 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,6 +14,6 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git luci-app-xray https://github.com/yichya/luci-app-xray' >>feeds.conf.default
+# echo 'src-git luci-app-xray https://github.com/yichya/luci-app-xray' >>feeds.conf.default
